d4eab47e077187166b1b664420fe71eff35f6df1
Salvador Guzman <guzmansalv@gmail.com>
2017-07-06 18:56:25 -0700

    added js react scaffolding

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const CounterDisplay = React.createClass({
+    propTypes: {
+        counterProp: React.PropTypes.number.isRequired,
+        inc: React.PropTypes.func.isRequired,
+        dec: React.PropTypes.func.isRequired
+    },
+
+    render: function () {
+        return React.createElement('div', null,
+            React.createElement('h2', null, this.props.counterProp),
+            React.createElement('button', {onClick: this.props.inc}, '+'),
+            React.createElement('button', {onClick: this.props.dec}, '-')
+        )
+    }
+});
+
+const Counter = React.createClass({
+    getDefaultProps: function () {
+        return {
+            name: 'Tom'
+        }
+    },
+
+    getInitialState: function () {
+        return {
+            counter: 0
+        }
+    },
+
+    inc: function () {
+        this.setState({counter: this.state.counter + 1});
+    },
+
+    dec: function () {
+        this.setState({counter: this.state.counter - 1});
+    },
+
+    render: function () {
+        return React.createElement('div', {},
+            React.createElement('h1', {}, this.props.name),
+            React.createElement(CounterDisplay, {counterProp: this.state.counter, inc: this.inc, dec: this.dec})
+        )
+    }
+});
+
+ReactDOM.render(
+    React.createElement(Counter), document.getElementById('react-mount-point')
+);
